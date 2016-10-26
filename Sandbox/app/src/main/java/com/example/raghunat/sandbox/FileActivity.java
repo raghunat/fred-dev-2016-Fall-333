2f17f957189209e09a44c436e8b54b756dd0995c
Stephen Raghunath <raghunat@fredonia.edu>
2016-10-26 14:42:38 -0400

    Adding file writing

--- Sandbox/app/src/main/java/com/example/raghunat/sandbox/FileActivity.java
+++ Sandbox/app/src/main/java/com/example/raghunat/sandbox/FileActivity.java
@@ -1,13 +1,18 @@
 package com.example.raghunat.sandbox;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class FileActivity extends AppCompatActivity {
 
@@ -17,19 +22,71 @@ public class FileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_file);
     }
 
+    protected void createFile(View view) {
+        // Create a file in the internal storage
+        String fileName = "example.txt";
+        String content = "I am inside the file!";
+        FileOutputStream stream;
+
+        try {
+            // Open/create the "file"
+            stream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            stream.write(content.getBytes());
+            stream.close();
+        } catch(Exception e) {
+            Log.e("Error", e.getMessage());
+        }
+    }
+
     protected void listFiles(View view) {
+        LinearLayout ll = (LinearLayout) findViewById(R.id.files_list);
+        ll.removeAllViews();
         // TODO List all internal files
         File internal = getFilesDir();
         // TODO List all cache files
         File cache = getCacheDir();
         // TODO List all external files
         File external = getExternalFilesDir(null);
-        // TODO Create TV's and insert into scroll view
-        String[] cacheList = cache.list();
-        LinearLayout ll = (LinearLayout)  findViewById(R.id.files_list);
-        for (int i = 0; i < cacheList.length; i++) {
-            TextView tv = new TextView(this);
-            tv.setText(cacheList[i]);
+        // Create TV's and insert into scroll view
+        // EXAMPLE FOR CACHE
+        //        String[] cacheList = cache.list();
+        //        LinearLayout ll = (LinearLayout)  findViewById(R.id.files_list);
+        //        for (int i = 0; i < cacheList.length; i++) {
+        //            TextView tv = new TextView(this);
+        //            tv.setText(cacheList[i]);
+        //            ll.addView(tv);
+        //        }
+
+        String[] files = internal.list();
+        final Context context = this;
+        for (int i = 0; i < files.length; i++) {
+            TextView tv = new TextView(this); // create TextView
+            tv.setText(files[i]); // set file name
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // get the text view
+                    TextView tv = (TextView)v;
+                    // Get the file and its content
+                    File clickedFile = new File(getFilesDir() + "/" + tv.getText());
+                    String content = "";
+                    try {
+                        FileInputStream stream = new FileInputStream(clickedFile);
+                        StringBuilder sb = new StringBuilder();
+                        while(stream.available() > 0) {
+                            sb.append((char)stream.read());
+                        }
+                        content = sb.toString();
+                    } catch (Exception e) {
+
+                    }
+                    // Make a toast!
+                    Toast toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+
+            // Text view is created, add it to the activity's view
             ll.addView(tv);
         }
      }
